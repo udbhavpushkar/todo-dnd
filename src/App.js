3459ea941a50c85ab11f292340dcdf05c9f7c743
udbhavpushkar <udbhavpushkar@gmail.com>
2021-04-15 19:12:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,36 +4,71 @@ import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 import _ from "lodash"
 import {v4} from "uuid"
 
-const item = {
-    id: v4(),
-    name: "first"
-}
-
-const item2 = {
-    id: v4(),
-    name: "second"
-}
-
-
-
 function App() {
+    const [job, setJob] = useState("")
   const [state, setState] = useState({
       "todo": {
           title: "Todo",
-          items: [item]
+          items: [],
+          color: "grey"
       },
       "in-progress":{
           title: "In Progress",
-          items: [item2]
+          items: [],
+          color: "green"
       },
       "done":{
           title: "Done",
-          items: []
+          items: [],
+          color: "blue"
       }
   });
+
+  const handleDragEnd = ({destination, source})=>{
+      if (!destination){
+          return
+      }
+
+      if (destination.index === source.index && destination.droppableId === source.droppableId){
+          return
+      }
+
+      const itemCopy = {...state[source.droppableId].items[source.index]}
+      setState(prev => {
+          prev = {...prev}
+          prev[source.droppableId].items.splice(source.index, 1)
+
+          prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
+          return prev
+      })
+  }
+
+  const addJob = ()=>{
+      setState(prevState => {
+          return{
+              ...prevState,
+              todo: {
+                  title: "Todo",
+                  items: [{
+                      id: v4(),
+                      name: job
+                  }, ...prevState.todo.items],
+                  color: "grey"
+              }
+          }
+      })
+      setJob("")
+  }
+
   return (
+    <>
+        <h1 style={{"textAlign": "center"}}>Board</h1>
+        <div className="input">
+            <input className="input-area" type="text" value={job} onChange={event => setJob(event.target.value)}/>
+            <button className="button" onClick={addJob}>Add</button>
+        </div>
     <div className="App">
-      <DragDropContext onDragEnd={e => console.log(e)}>
+      <DragDropContext onDragEnd={handleDragEnd}>
           {_.map(state, (data, key)=>{
               return(
                   <div key={key} className="column">
@@ -41,13 +76,14 @@ function App() {
                       <Droppable droppableId={key}>
                           {(provided)=>{
                               return(
-                                  <div ref={provided.innerRef} {...provided.droppableProps} className="droppable-col">
+                                  <div ref={provided.innerRef} {...provided.droppableProps} className={`droppable-col ${data.color}`}>
                                       {data.items.map((ele, index)=>{
                                           return(
                                               <Draggable key={ele.id} index={index} draggableId={ele.id}>
                                                   {(provided)=>{
                                                       return(
                                                           <div
+                                                              className="item"
                                                               ref={provided.innerRef}
                                                               {...provided.draggableProps}
                                                               {...provided.dragHandleProps}>
@@ -58,6 +94,7 @@ function App() {
                                               </Draggable>
                                           )
                                       })}
+                                      {provided.placeholder}
                                   </div>
                               )
                           }}
@@ -67,6 +104,7 @@ function App() {
           })}
       </DragDropContext>
     </div>
+    </>
   );
 }
 
